e5e7871694260ccee93e553a758dff2c030ded95
Brzhk <berzehk@gmail.com>
2019-06-07 03:00:45 +0200

    remove hazelcast-kubernetes

---
+++ src/main/java/com/mycompany/myapp/config/MetricsConfiguration.java
@@ -0,0 +1,21 @@
+package com.mycompany.myapp.config;
+
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.binder.MeterBinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfiguration {
+
+    @Bean
+    public MeterBinder processMemoryMetrics() {
+        return new ProcessMemoryMetrics();
+    }
+
+    @Bean
+    public MeterBinder processThreadMetrics() {
+        return new ProcessThreadMetrics();
+    }
+}
